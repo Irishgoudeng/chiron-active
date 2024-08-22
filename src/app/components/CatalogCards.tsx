63838cd2f185db5b7c2fa925270a89057106673e
irishgoudeng <vincewarpie@gmail.com>
2024-08-22 18:07:53 +0800

    This is in Development

--- src/app/components/CatalogCards.tsx
+++ src/app/components/CatalogCards.tsx
@@ -14,7 +14,7 @@ const CatalogCards = () => {
   ];
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 p-4 mx-8">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 p-4 mx-4">
       {items.map((item) => (
         <div key={item.id}>
           <div className="bg-white shadow-md overflow-hidden">
